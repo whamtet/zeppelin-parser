6be7ab176a806e92ef958466be862043e7c6152a
Matthew Molloy <whamtet@gmail.com>
2016-05-28 10:17:46 +0800

    improved parser

--- src/zeppelin_parser/core.clj
+++ src/zeppelin_parser/core.clj
@@ -13,9 +13,9 @@
   (insta/parser "
                 start = prefix assignment *
                 prefix = (!'val' wildcard) *
-                assignment = 'val' whitespace symbol (!'val' wildcard) +
+                assignment = val symbol (!val wildcard) +
+                val = 'val' #'\\s+'
                 wildcard = #'[\\w\\W]'
-                whitespace = #'\\s*'
                 symbol = #'\\w+'
                 "))
 
@@ -30,22 +30,28 @@
            (interpose "\n"
                       (map strip-comments (.split text "\n"))))))
 
-(defn get-assignment [[_ _ _ [_ val] & assigment]]
+(defn get-assignment [[_ _ [_ val] & assigment :as x]]
   (let [
          assigment (apply str (map second assigment))
          ]
     [val assigment]))
 
-(defn get-assigments [s] (map get-assignment (drop 2 (parse s))))
+(defn get-assigments [s]
+  (when (.contains s "val ")
+    (when-not (every? first (map get-assignment (drop 2 (parse s)))) (println "bad" s (drop 2 (parse s))))
+    (map get-assignment (drop 2 (parse s)))))
 
 (defn value-map [f m]
   (zipmap (keys m) (map f (vals m))))
 
+(defn parse-paragraphs [f]
+  (mapcat get-assigments (filter identity (map clean-code ((json/read-str (slurp f)) "paragraphs")))))
+
 (defn get-m [f]
   (let [
-         paragraphs (filter identity (map clean-code ((json/read-str (slurp f)) "paragraphs")))
-         assignments (into {} (mapcat get-assigments paragraphs))
+         assignments (into {} (parse-paragraphs f))
          symbols (keys assignments)
+         _ (assert (every? identity symbols))
          symbol-assignments (value-map (fn [s] (filter #(.contains s %) symbols)) assignments)
          ]
     symbol-assignments))
@@ -56,10 +62,16 @@
 (defn open-graph [f]
   (let [m (get-m f)]
     (viz/save-graph (keys m) m
-                    :node->descriptor (fn [n] {:label n})
-                    :vertical? true
-                    :filename "graph.png")
+                      :node->descriptor (fn [n] {:label n})
+                      :vertical? true
+                      :filename "graph.png")
     (open "graph.png")))
+
+(defn count-paragraph [{text "text"}]
+  (if text (count (.split text "\n")) 0))
+
+(defn count-lines [f]
+  (apply + (map count-paragraph ((json/read-str (slurp f)) "paragraphs"))))
 
 (defn -main [& [f]]
   (if f (open-graph f) (println "Usage: java -jar zeppelin-graph.jar notebook.json")))
